6530ccf63ae881888f07b793abdb94982cf5a037
Abhibhav Sharma <139039304+Abhibhav2003@users.noreply.github.com>
2024-04-18 15:53:06 +0530

    Update ActivitySelect.cpp

--- ActivitySelect.cpp
+++ ActivitySelect.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+using namespace std;
+
+struct Activity {
+    int start, finish, index;
+};
+
+bool compareActivities(const Activity& a, const Activity& b) {
+    return a.finish < b.finish;
+}
+
+void activitySelection(const vector<Activity>& activities) {
+    cout << "Selected Activities:\n";
+
+    vector<Activity> sortedActivities = activities;
+    sort(sortedActivities.begin(), sortedActivities.end(), compareActivities);
+
+    
+    cout << "(" << sortedActivities[0].start << ", " << sortedActivities[0].finish << ") ";
+
+    int lastSelected = 0;
+
+    
+    for (int i = 1; i < sortedActivities.size(); ++i) {
+        if (sortedActivities[i].start >= sortedActivities[lastSelected].finish) {
+            cout << "(" << sortedActivities[i].start << ", " << sortedActivities[i].finish << ") ";
+            lastSelected = i;
+        }
+    }
+
+    cout << "\n";
+}
+
 
